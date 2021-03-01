609e9636f5fc2e7c28c3c175c5631c73bb908bce
Muntari29 <muntrock@gmail.com>
2021-03-01 22:55:30 +0900

    algo_interview_3

--- Python/Python_100/43_python_binary_decimel.py
+++ Python/Python_100/43_python_binary_decimel.py
@@ -1,2 +1,18 @@
 # 10진수를 2진수로 바꾸는 프로그램
 # ex) 13 -> 1101
+
+number = int(input())
+
+binary = ''
+
+while number > 0: 
+    share = number // 2 
+    reminder = number % 2
+    binary += str(reminder)
+    number = share
+
+print(binary[::-1])
+# 1101
+
+print(bin(10))
+# 1101 python bin 함수 이용하기 10진수 -> 2진수로 표현해줌
