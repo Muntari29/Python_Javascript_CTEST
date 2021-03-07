ac9a9a586b1542158993bdcaacdf5e1772ac10ef
Muntari29 <65607601+Muntari29@users.noreply.github.com>
2021-03-07 22:48:20 +0900

    Javascript_CTEST_1 (#18)(squash)

---
+++ Javascript/4_js_typeof_2.js
@@ -0,0 +1,19 @@
+// 변수의 타입!
+// 올바른 변수의 타입
+
+const arr1 = 1;
+// number
+const arr2 = 2.22;
+// number
+const arr3 = "p";
+// string
+const arr4 = [1, 2, 3];
+// object
+const arr5 = ["A", "B", "C"];
+// object
+
+console.log(typeof(arr1));
+console.log(typeof(arr2));
+console.log(typeof(arr3));
+console.log(typeof(arr4));
+console.log(typeof(arr5));
