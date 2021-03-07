896caaea80b491b2acccb89a82739120ea09e7c8
Muntari29 <muntrock@gmail.com>
2021-03-07 22:47:16 +0900

    Javascript_CTEST_1

---
+++ Javascript/1_js_array_delete.js
@@ -0,0 +1,8 @@
+// 배열 삭제하기
+// 400, 500 요소 삭제하기
+
+var numbers = [100, 200, 300, 400, 500];
+
+numbers.pop();
+numbers.pop();
+console.log(numbers);
---
+++ Javascript/5_js_basic_for.js
@@ -0,0 +1,29 @@
+// for 문 기초!!
+/*
+for ([초기문]; [조건문]; [증감문])
+  문장
+  실행 순서 초기, 조건 , 문장 , 증감 반복힘
+*/
+
+var a = 10;
+var b = 2;
+
+// for(var i = 1; i < 5; i += 2){
+//     console.log(i)
+//     a += i;
+// }
+
+// console.log(a + b);
+// // 16
+
+// // 24 만들기!!
+var c = 10;
+var d = 2;
+
+for(var i = 0; i < 7; i += 2){
+    console.log(i)
+    c += i;
+}
+
+console.log(c+d);
+// 24
