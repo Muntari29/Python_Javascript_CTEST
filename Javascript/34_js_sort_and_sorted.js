9bcf30180fcef48617491243c6cc545c0156fb59
Muntari29 <muntrock@gmail.com>
2021-03-13 21:08:25 +0900

    Javascript_94_2

---
+++ Javascript/34_js_sort_and_sorted.js
@@ -0,0 +1,25 @@
+/*
+sort 구현하기
+키가 주어지면 순서대로 제대로 섰는지 확인하는 프로그램 만들기 
+ex) 176 156 155 165 166 169
+YES or NO
+*/
+
+// // const data = prompt("문자를 입력해주세요!").split(" ");
+// const data = ["176", "156", "155", "165", "166", "169"];
+
+// const sortData = data;
+
+// sortData.sort(function(a, b){
+//     return a - b;
+// })
+
+// console.log(data)
+// console.log(sortData)
+
+// // if (data === sortData){
+// //     console.log("YES")
+// // } else {
+// //     console.log("NO")
+// // }
+// // if (data == )
