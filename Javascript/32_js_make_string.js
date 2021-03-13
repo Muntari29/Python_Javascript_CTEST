f255b9f1f2e44061f0e4d444ff5f70a0296951bf
Muntari29 <65607601+Muntari29@users.noreply.github.com>
2021-03-13 21:09:34 +0900

    Javascript_CTEST_94 (#24)(squash)

---
+++ Javascript/32_js_make_string.js
@@ -0,0 +1,14 @@
+// 단어의 갯수를 출력하는 프로그램 만들기
+// ex) 입력 : 안녕하세요. 저는 제주대학교 컴퓨터공학전공 혜림입니다.
+//     출력 : 5
+
+// const data = prompt("문장 입력하세요").split(" ")
+
+// console.log(data.length)
+
+
+const data = "안녕하세요. 저는 제주대학교 컴퓨터공학전공 혜림입니다."
+
+const result = data.split(" ")
+
+console.log(result.length);
