63620d7aa4207e025b9e013026fa2ca091c62ef3
Muntari29 <65607601+Muntari29@users.noreply.github.com>
2021-04-15 18:19:03 +0900

    DS Stack 문서 수정 (#39)

--- DataStructure/def_stack.py
+++ DataStructure/def_stack.py
@@ -3,3 +3,20 @@
 from collections import deque
 
 A = deque()
+
+# 맨 뒤 데이터 추가
+A.append(1)
+A.append(22)
+A.append(333)
+
+# 맨 앞 삭제
+A.popleft()
+
+# 맨 뒤 삽입
+A.append(300)
+
+# 맨 뒤 삭제
+A.pop()
+
+# 길이 확인
+len(A)
