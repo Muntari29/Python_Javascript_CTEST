e0ad878f54539676d6e55129acd4580a97f46138
Muntari29 <muntrock@gmail.com>
2021-04-22 14:14:20 +0900

    DS hip 우선순위 큐 (우선순위 데이터 추출) 구현

---
+++ DataStructure/def_hip_pick_priority_data.py
@@ -0,0 +1,48 @@
+# 힙 우선순위 큐 
+# 우선순위 데이터 추출 (삭제)
+
+# 이전 메서드 재사용
+from hip_insert import reverse_heapify
+from hip_sort import swap, heapify
+
+class PriorityQueue:
+    """힙으로 구현한 우선순위 큐"""
+    def __init__(self):
+        self.heap = [None]  # 파이썬 리스트로 구현한 힙
+
+    def insert(self, data):
+        """삽입 메소드"""
+        self.heap.append(data)  # 힙의 마지막에 데이터 추가
+        reverse_heapify(self.heap, len(self.heap)-1) # 삽입된 노드(추가된 데이터)의 위치를 재배치
+
+    def extract_max(self):
+        """최우선순위 데이터 추출 메소드"""
+        swap(self.heap, 1, -1)
+        result = self.heap.pop()
+        heapify(self.heap, 1, len(self.heap))
+        return result
+        
+
+    def __str__(self):
+        return str(self.heap)
+
+# 출력 코드
+priority_queue = PriorityQueue()
+
+priority_queue.insert(6)
+priority_queue.insert(9)
+priority_queue.insert(1)
+priority_queue.insert(3)
+priority_queue.insert(10)
+priority_queue.insert(11)
+priority_queue.insert(13)
+
+# [None, 13, 9, 11, 3, 6, 1, 10]
+
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
+print(priority_queue.extract_max())
