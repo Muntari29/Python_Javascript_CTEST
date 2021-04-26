708ccbddc6d9596dad47ce55caa712f627bf31fc
Muntari29 <65607601+Muntari29@users.noreply.github.com>
2021-04-26 17:54:32 +0900

    DS 이진탐색트리 삭제 경우의수_2 (#57)

--- DataStructure/def_binary_search_tree_delete.py
+++ DataStructure/def_binary_search_tree_delete.py
@@ -22,7 +22,56 @@ class BinarySearchTree:
             # 지우려는 노드가 부모 노드의 오른쪽 자식 노드인 경우    
             else:
                 parent_node.right_child = None
-        
+    
+        # 경우 2 : 지우려는 노드가 하나의 자식만 있는 노드인 경경우
+        # 2- 1 : 지우려는 노드가 왼쪽 자식 노드만 가지고 있는 경우
+        elif node_to_delete.left_child is not None and node_to_delete.right_child is None:
+            # 지우려는 노드가 왼쪽 자식만 가진 루트 노드일 경우
+            if node_to_delete is self.root:
+                self.root = node_to_delete.left_child
+                self.root.parent = None
+            # 지우려는 노드가 부모 노드에 왼쪽에 위치하는 경우
+            elif node_to_delete.data < parent_node.data:
+                parent_node.left_child = node_to_delete.left_child
+                node_to_delete.left_child.parent = parent_node
+            # 지우려는 노드가 부모 노드에 오른쪽에 위치하는 경우
+            else:
+                parent_node.right_child = node_to_delete.left_child
+                node_to_delete.left_child.parent = parent_node
+                
+        # 2-2 : 지우려는 노드가 오른쪽 자식 노드만 가지고 있는 경우
+        else:
+            # 지우려는 노드가 오른쪽 자시만 가진 루트 노드일 경우
+            if node_to_delete is self.root:
+                self.root = node_to_delete.right_child
+                self.root.parent = None
+            # 지우려는 노드가 부모 노드에 왼쪽에 위치하는 경우
+            elif node_to_delete.data > parent_node.data:
+                parent_node.left_child = node_to_delete.right_child
+                node_to_delete.right_child.parent = parent_node
+            # 지우려는 노드가 부모 노드에 오른쪽에 위치하는 경우
+            else:
+                parent_node.right_child = node_to_delete.right_child
+                node_to_delete.right_child.parent = parent_node
+
+    def search(self, data):
+        """이진 탐색 트리 탐색 메소드, 찾는 데이터를 갖는 노드가 없으면 None을 리턴한다"""
+        temp = self.root  # 탐색용 변수, root 노드로 초기화
+    
+        # 원하는 데이터를 갖는 노드를 찾을 때까지 돈다
+        while temp is not None:
+            # 원하는 데이터를 갖는 노드를 찾으면 리턴
+            if data == temp.data:
+                return temp
+            # 원하는 데이터가 노드의 데이터보다 크면 오른쪽 자식 노드로 간다
+            if data > temp.data:
+                temp = temp.right_child
+            # 원하는 데이터가 노드의 데이터보다 작으면 왼쪽 자식 노드로 간다
+            else:
+                temp = temp.left_child
+    
+        return None # 원하는 데이터가 트리에 없으면 None 리턴
+
 
     @staticmethod
     def find_min(node):
