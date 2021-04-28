df070d8ef52393b002b0ef69b8d5c837ef9dde67
Muntari29 <65607601+Muntari29@users.noreply.github.com>
2021-04-28 14:57:47 +0900

    pro_5 추가 (#61)

--- Python/Algorithm_interview/pro_stack_queue.py
+++ Python/Algorithm_interview/pro_stack_queue.py
@@ -25,4 +25,20 @@ def solution(prices):
 
     return answer
 
-    
+# [1,2,3,2,3]
+def solution2(prices):
+    # 인덱스 에러가 발생하지 않도록 인덱스 초기화를 해준다!
+    answer = [0] * len(prices)
+    stack = [0]
+    for i in range(1, len(prices)):
+        if prices[i] < prices[stack[-1]]:
+            for j in stack[::-1]:
+                if prices[i] < prices[j]:
+                    answer[j] = i-j
+                    stack.remove(j)
+                else:
+                    break
+        stack.append(i)
+    for i in range(0, len(stack)-1):
+        answer[stack[i]] = len(prices) - stack[i] - 1
+    return answer
