f536366b1161fe2474b62157f47953900a214d19
Muntari29 <muntrock@gmail.com>
2021-04-30 20:59:16 +0900

    DS 그래프_인접리스트로 구현하기

--- DataStructure/used_dict_def_graph/def_graph_use_dict.py
+++ DataStructure/used_dict_def_graph/def_graph_use_dict.py
@@ -1,31 +1,22 @@
-# 딕셔너리를 활용하여 그래프 구현하기
-
 class StationNode:
     """간단한 지하철 역 노드 클래스"""
     def __init__(self, station_name):
         self.station_name = station_name
-
-def create_station_nodes(input_file):
-    """input_file에서 데이터를 읽어 와서 지하철 그래프 노드들을 리턴하는 함수"""
-    stations = {}  # 지하철 역 노드들을 담을 딕셔너리
-
-    # 파라미터로 받은 input_file(문자열타입) 해당 이름을 갖는파일을 연다
-    with open(input_file) as stations_raw_file:
-        for line in stations_raw_file:  # 파일을 한 줄씩 받아온다
-            # 앞 뒤 공백 삭제후, -를 기준으로 나눈 리스트로 만듬.
-            subway_line = line.strip().split("-")
-            
-            # subway_line 배열을 반복
-            # 요소별로 양옆의 여백(띄어쓰기)를 다시 한번 제거
-            for name in subway_line:
-                station_name = name.strip()
-                stations[station_name] = StationNode(station_name)
-
-    return stations
+        self.adjacent_stations = []  # 인접 리스트
 
 
-stations = create_station_nodes("/Users/munseunghui/playground/codingtest/Python_Javascript_CTEST/DataStructure/used_dict_def_graph/test_stations.txt")  # stations.txt 파일로 그래프 노드들을 만든다
+    def add_connection(self, other_station):
+        """지하철 역 노드 사이 엣지 저장하기"""
+        self.adjacent_stations.append(other_station)
+        other_station.adjacent_stations.append(self)
 
-# stations에 저장한 역들 이름 출력 (채점을 위해 역 이름 순서대로 출력)
-for station in sorted(stations.keys()):
-    print(stations[station].station_name)
+
+    def __str__(self):
+        """지하철 노드 문자열 메소드. 지하철 역 이름과 연결된 역들을 모두 출력해준다"""
+        res_str = f"{self.station_name}: "  # 리턴할 문자열
+
+        # 리턴할 문자열에 인접한 역 이름들 저장
+        for station in self.adjacent_stations:
+            res_str += f"{station.station_name} "
+  
+        return res_str
