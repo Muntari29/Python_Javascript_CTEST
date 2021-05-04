a22e40ab698440cfce04c0449e8079de84905ebf
Muntari29 <65607601+Muntari29@users.noreply.github.com>
2021-05-04 15:54:51 +0900

    DS BFS 탐색 알고리즘 파일 수정 (#71)

--- DataStructure/graph_BFS/subway.graph.py
+++ DataStructure/graph_BFS/subway.graph.py
@@ -1,51 +1,40 @@
-from collections import deque
-from subway_graph import create_station_graph
+# BFS
 
-def bfs(graph, start_node):
-    """시작 노드에서 bfs를 실행하는 함수"""
-    # 이 함수의 결과로 주어진 노드(탐색기준)과 인접해있는 노드들은 방문표시가 True로 변경되며
-    # True로 변경됨은 주어진 노드와 연결(경로)되어있는다는 의미이다.
-    queue = deque()  # 빈 큐 생성
+class StationNode:
+    """지하철 역을 나타내는 역"""
+    def __init__(self, station_name):
+        self.station_name = station_name
+        self.adjacent_stations = []
+        self.visited = False
 
-    # 탐색을 한번만하지 않기에, 일단 모든 노드를 방문하지 않은 노드로 표시
-    for station_node in graph.values():
-        station_node.visited = False
-    
-    # 시작점 노드를 방문 표시 해줌
-    start_node.visited = True
-     # 큐에 시작점 노드를 넣은 후 반복문 시작
-    queue.append(start_node)
-    
-    # 큐에 노드가 없을때까지 반복
-    while queue:
-        # 큐의 맨앞 데이터 추출
-        queue_frist_node = queue.popleft()
-
-        # 맨앞 추출한 데이터(여기선 노드) 를 이용해 
-        # 추출한 노드의 인접해있는 노드들을 돌면서 방문 여부를 변경함
-        # 만약 방문하지 않은(false)일 경우 큐에 넣고 큐에 노드가 없을때까지 반반복
-        for adjacent_node in queue_frist_node.adjacent_stations:
-            if not adjacent_node.visited:
-                adjacent_node.visited = True
-                queue.append(adjacent_node)
+    def add_connection(self, station):
+        """파라미터로 받은 역과 엣지를 만들어주는 메소드"""
+        self.adjacent_stations.append(station)
+        station.adjacent_stations.append(self)
 
 
-stations = create_station_graph("./new_stations.txt")  # stations.txt 파일로 그래프를 만든다
+def create_station_graph(input_file):
+    stations = {}
 
-# stations 딕셔너리의 키 = 강남과 일치하는 value, 즉 station_node를 저장
-gangnam_station = stations["강남"]
+    # 일반 텍스트 파일을 여는 코드
+    with open(input_file) as stations_raw_file:
+        for line in stations_raw_file:  # 파일을 한 줄씩 받아온다
+            previous_station = None  # 엣지를 저장하기 위한 변수
+            raw_data = line.strip().split("-")
 
-# 강남역과 경로를 통해 연결된 모든 노드를 탐색
-bfs(stations, gangnam_station)
+            for name in raw_data:
+                station_name = name.strip()
 
-# 강남역과 서울 지하철 역들이 연결됐는지 확인
-print(stations["강동구청"].visited)
-print(stations["평촌"].visited)
-print(stations["송도"].visited)
-print(stations["개화산"].visited)
+                if station_name not in stations:
+                    current_station = StationNode(station_name)
+                    stations[station_name] = current_station
 
-# 강남역과 대전 지하철 역들이 연결됐는지 확인
-print(stations["반석"].visited)
-print(stations["지족"].visited)
-print(stations["노은"].visited)
-print(stations["(대전)신흥"].visited)
+                else:
+                    current_station = stations[station_name]
+
+                if previous_station is not None:
+                    current_station.add_connection(previous_station)
+
+                previous_station = current_station
+
+    return stations
--- DataStructure/graph_BFS/use_BFS_search.py
+++ DataStructure/graph_BFS/use_BFS_search.py
@@ -1,17 +1,40 @@
+# BFS 탐색 알고리즘 구현
+
 from collections import deque
 from subway_graph import create_station_graph
 
 def bfs(graph, start_node):
     """시작 노드에서 bfs를 실행하는 함수"""
+    # 이 함수의 결과로 주어진 노드(탐색기준)과 인접해있는 노드들은 방문표시가 True로 변경되며
+    # True로 변경됨은 주어진 노드와 연결(경로)되어있는다는 의미이다.
     queue = deque()  # 빈 큐 생성
 
-    # 일단 모든 노드를 방문하지 않은 노드로 표시
+    # 탐색을 한번만하지 않기에, 일단 모든 노드를 방문하지 않은 노드로 표시
     for station_node in graph.values():
         station_node.visited = False
+    
+    # 시작점 노드를 방문 표시 해줌
+    start_node.visited = True
+     # 큐에 시작점 노드를 넣은 후 반복문 시작
+    queue.append(start_node)
+    
+    # 큐에 노드가 없을때까지 반복
+    while queue:
+        # 큐의 맨앞 데이터 추출
+        queue_frist_node = queue.popleft()
+
+        # 맨앞 추출한 데이터(여기선 노드) 를 이용해 
+        # 추출한 노드의 인접해있는 노드들을 돌면서 방문 여부를 변경함
+        # 만약 방문하지 않은(false)일 경우 큐에 넣고 큐에 노드가 없을때까지 반반복
+        for adjacent_node in queue_frist_node.adjacent_stations:
+            if not adjacent_node.visited:
+                adjacent_node.visited = True
+                queue.append(adjacent_node)
 
 
 stations = create_station_graph("./new_stations.txt")  # stations.txt 파일로 그래프를 만든다
 
+# stations 딕셔너리의 키 = 강남과 일치하는 value, 즉 station_node를 저장
 gangnam_station = stations["강남"]
 
 # 강남역과 경로를 통해 연결된 모든 노드를 탐색
